b64734006d15b98c680378edfcb58135739552ea
Jenna Wasserman <jennaw@amazon.com>
2017-10-04 14:39:33 -0700

    Commiting Checkout.java, a class to handle encapsulate the checkout transaction details.

--- src/Checkout.java
+++ src/Checkout.java
@@ -1,4 +1,23 @@
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 
 public class Checkout {
+    private long iD;
+    private Checkoutable item;
+    private Calendar checkoutDate;
+
+    public Checkout(long iD, Checkoutable item, Calendar checkoutDate) {
+        this.iD = iD;
+        this.item = item;
+        this.checkoutDate = checkoutDate;
+    }
+    
+    public boolean isLate() {
+        Calendar currentDate = Calendar.getInstance();
+        if(ChronoUnit.DAYS.between(currentDate.toInstant(), checkoutDate.toInstant()) > item.getCheckoutPeriod()) {
+            return true;
+        }
+        return false;
+    }
 
 }
